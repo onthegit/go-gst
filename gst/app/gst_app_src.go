dd866a85689c565bd018358a74a81dac8313d421
onthegit <43019496+onthegit@users.noreply.github.com>
2023-09-10 22:37:31 +0200

    Update

--- gst/app/gst_app_src.go
+++ gst/app/gst_app_src.go
@@ -18,7 +18,6 @@ gboolean  cgoSeekDataCb   (GstAppSrc *src, guint64 offset, gpointer user_data) {
 */
 import "C"
 import (
-	"runtime"
 	"unsafe"
 
 	"github.com/go-gst/go-gst/gst"
@@ -138,10 +137,6 @@ func (a *Source) PushBytes(buf []byte) gst.FlowReturn {
 
 	p := C.g_memdup2((C.gconstpointer)(b), C.ulong(len(buf)))
 	buffer := C.gst_buffer_new_wrapped(p, C.ulong(len(buf)))
-
-	runtime.SetFinalizer(buffer, func(buf *C.GstBuffer) {
-		C.gst_buffer_unref(buf)
-	})
 
 	return gst.FlowReturn(C.gst_app_src_push_buffer((*C.GstAppSrc)(a.Instance()), buffer))
 }
